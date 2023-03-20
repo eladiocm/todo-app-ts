a2723c736ecfabfd4436360616afe18efca0b172
eladiocm <eladio970826@gmail.com>
2023-03-19 20:54:03 -0400

    show todo list

---
+++ src/components/Todos/Todos.tsx
@@ -0,0 +1,26 @@
+import { type ListOfTodo } from '../../types'
+import { Todo } from './Todo'
+
+interface Props {
+  todos: ListOfTodo
+}
+
+export const Todos: React.FC<Props> = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li
+          key={todo.id}
+          className={`${todo.completed ? 'completed' : ''}`}
+        >
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
